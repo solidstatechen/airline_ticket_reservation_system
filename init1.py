1313a2f8607c68e8407b17c942afeb924ffaa591
Oli <oli@Olis-Macbook-Pro.local>
2020-12-03 15:16:04 +0000

    all flights displayed at index.html and at home.html(logged in)

--- init1.py
+++ init1.py
@@ -17,108 +17,112 @@ conn = mysql.connector.connect(host='localhost',
 #Define a route to hello function
 @app.route('/')
 def hello():
-	return render_template('index.html')
+    cursor = conn.cursor();
+    query = "SELECT * FROM flight ORDER BY departure_time DESC"
+    cursor.execute(query)
+    data1 = cursor.fetchall() 
+    cursor.close()
+    return render_template('index.html', flights=data1)
 
 #Define route for login
 @app.route('/login')
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
 
 #Define route for register
 @app.route('/register')
 def register():
-	return render_template('register.html')
+    return render_template('register.html')
 
 #Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
+    #grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
 
-	#cursor used to send queries
-	cursor = conn.cursor()
-	#executes query
-	query = "SELECT * FROM user WHERE username = \'{}\' and password = \'{}\'"
-	cursor.execute(query.format(username, password))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-	cursor.close()
-	error = None
-	if(data):
-		#creates a session for the the user
-		#session is a built in
-		session['username'] = username
-		return redirect(url_for('home'))
-	else:
-		#returns an error message to the html page
-		error = 'Invalid login or username'
-		return render_template('login.html', error=error)
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = "SELECT * FROM user WHERE username = \'{}\' and password = \'{}\'"
+    cursor.execute(query.format(username, password))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if(data):
+        #creates a session for the the user
+        #session is a built in
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        #returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
 
 #Authenticates the register
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
-	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
+    #grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
 
-#	if not len(password) >= 4:
+#   if not len(password) >= 4:
 #                flash("Password length must be at least 4 characters")
  #               return redirect(request.url)
 
-	#cursor used to send queries
-	cursor = conn.cursor()
-	#executes query
-	query = "SELECT * FROM user WHERE username = \'{}\'"
-	cursor.execute(query.format(username))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-	error = None
-	if(data):
-		#If the previous query returns data, then user exists
-		error = "This user already exists"
-		return render_template('register.html', error = error)
-	else:
-		ins = "INSERT INTO user VALUES(\'{}\', \'{}\')"
-		cursor.execute(ins.format(username, password))
-		conn.commit()
-		cursor.close()
-		flash("You are logged in")
-		return render_template('index.html')
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = "SELECT * FROM user WHERE username = \'{}\'"
+    cursor.execute(query.format(username))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    error = None
+    if(data):
+        #If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error = error)
+    else:
+        ins = "INSERT INTO user VALUES(\'{}\', \'{}\')"
+        cursor.execute(ins.format(username, password))
+        conn.commit()
+        cursor.close()
+        flash("You are logged in")
+        return render_template('index.html')
 
 @app.route('/home')
 def home():
-    username = session['username']
     cursor = conn.cursor();
-    query = "SELECT * FROM flight WHERE username = \'{}\' ORDER BY ts DESC"
-    cursor.execute(query.format(username))
+    query = "SELECT * FROM flight ORDER BY departure_time DESC"
+    cursor.execute(query)
     data1 = cursor.fetchall() 
     cursor.close()
-    return render_template('home.html', username=username)
+    return render_template('home.html', flights=data1)
 
-	
+    
 @app.route('/search', methods=['GET', 'POST'])
 def post():
-	username = session['username']
-	cursor = conn.cursor();
-	flight = request.form['flight']
-	query = "SELECT * FROM flight (flight_num, status) VALUES(\'{}\', \'{}\')"
-	cursor.execute(query.format(flight, username))
-	conn.commit()
-	cursor.close()
-	return redirect(url_for('home'))
+    username = session['username']
+    cursor = conn.cursor();
+    flight = request.form['flight']
+    query = "SELECT * FROM flight (flight_num, status) VALUES(\'{}\', \'{}\')"
+    cursor.execute(query.format(flight, username))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
 
 
 @app.route('/logout')
 def logout():
-	session.pop('username')
-	return redirect('/')
-		
+    session.pop('username')
+    return redirect('/')
+        
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
 #debug = True -> you don't have to restart flask
 #for changes to go through, TURN OFF FOR PRODUCTION
 if __name__ == "__main__":
-	app.run('127.0.0.1', 5000, debug = True)
+    app.run('127.0.0.1', 5000, debug = True)
